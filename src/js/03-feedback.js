f3cc5c82a36b87fb3871186cfa7d7820ea5b22eb
timechain1 <andrey.lutsiak@gmail.com>
2022-10-20 17:17:14 +0300

    the third

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,42 @@
 import throttle from 'lodash.throttle';
 
-const CHAIN_KEY = 'feedback-form-state';
-const formData = {};
-const form = document.querySelector('.feedback-form');
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
-const reset = () => {
-    email.value = '';
-    message.value = '';
-    localStorage.removeItem(CHAIN_KEY);
-  };
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
+  //   btn: document.querySelector('button[typea="submit]'),
+};
+const FEEDBACK_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(FEEDBACK_KEY)) || {};
+showSavedText();
 
-if (localStorage.hasOwnProperty(CHAIN_KEY)) {
-  const data = JSON.parse(localStorage.getItem(CHAIN_KEY));
-  email.value = data.email;
-  message.value = data.message;
+refs.form.addEventListener('input', throttle(saveValue, 500));
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function saveValue(e) {
+  formData[e.target.name] = e.target.value;
+  //   console.log(formData);
+  //   console.log(formDataJSON);
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
-form.addEventListener('submit', OnBasedSubmit);
-const timing = throttle(() => {
-  formData.email = email.value;
-  formData.message = message.value;
-  localStorage.setItem(CHAIN_KEY, JSON.stringify(formData));
-}, 500);
-form.addEventListener('input', timing);
-
-function OnBasedSubmit(e) {
+function onFormSubmit(e) {
   e.preventDefault();
-  console.log(formData);
-  form.reset();
-  localStorage.removeItem(CHAIN_KEY);
+  if (refs.email.value && refs.message.value) {
+    e.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem(FEEDBACK_KEY)));
+    localStorage.removeItem(FEEDBACK_KEY);
+  }
+}
+
+function showSavedText() {
+  const savedDataJSON = localStorage.getItem(FEEDBACK_KEY);
+  const savedData = JSON.parse(savedDataJSON);
+  //   console.log(savedData.message);
+  if (savedData) {
+    refs.email.value = savedData.email || '';
+    refs.message.value = savedData.message || '';
+  }
 }
 
